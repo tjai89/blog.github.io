6395aaa0dfab09545c70d3ba4083c3c603405852
Kirill Zhuravlov <tjai89@mail.ru>
2015-12-08 20:27:19 +0300

    finishing friendly_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,11 +64,11 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :slug)
     end
 end
